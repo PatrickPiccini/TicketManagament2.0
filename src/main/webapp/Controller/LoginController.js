a77a746f82f9f7ef088d927ba41ab8231f0c91c5
Patrick Piccini <patrick.piccini@compasso.com.br>
2021-05-28 17:53:49 -0300

    ajuste de request para API

--- src/main/webapp/Controller/LoginController.js
+++ src/main/webapp/Controller/LoginController.js
@@ -1,15 +1,65 @@
 const request = require("request");
 
-async function sendToLogin(request, respose){
-    var user = document.getElementsByName("username").values
-    var password = document.getElementsByName("password").values
+var SEND_BANCO = process.env.SEND_BANCO || "coloque a url do caminho do banco"
+
+function sendToLogin() {
+    var user = document.querySelector('#name').value
+    var password = document.querySelector('#pass').value
 
     var credentials = {
-        Usuario:user,
-        Senha:password
+        Usuario: user,
+        Senha: password
     }
-    console.log(credentials.body);
- 
+    console.log(credentials);
+
+    // var BancoResult = () => {
+    //     request(`${SEND_BANCO}/`, credentials, (error,response) => {
+    //     if(error) throw(error)
+    //     return response
+    // })}
+    BancoResult = true
+
+    BancoResult == true ? window.location.href = "file:///C:/Users/patrick.piccini/Desktop/TicketManagament2.0/src/main/webapp/home.html"
+        : window.location.href = "file:///C:/Users/patrick.piccini/Desktop/TicketManagament2.0/src/main/webapp/fail.html"
 }
 
+function sendToCadastro() {
+    var name = document.querySelector('#name').value
+    var last_name = document.querySelector('#sobrenome').value
+    var email = document.querySelector('#email').value
+    var email_verify = document.querySelector('#verifyemail').value
+    var password = document.querySelector('#senha').value
+    var password_verify = document.querySelector('#pass').value
 
+    var stap = true
+    while (stap == true)
+        if (email == email_verify && password == password_verify) {
+            alert('deu certo!!!!!!!!!')
+            stap = false
+        } else {
+            alert('SENHA OU EMAIL INVÁLIDO')
+                (password_verify && password && email_verify && email).value = ''
+            stap = true
+        }
+
+    
+    var credentials = {
+        name,
+        last_name,
+        email,
+        email_verify,
+        password,
+        password_verify
+    }
+
+    // var BancoResultRegister = () => {
+    //     request(`${SEND_BANCO}/`, credentials, (error,response) => {
+    //     if(error) throw(error)
+    //     return response
+    // })}
+
+    BancoResultRegister = true
+
+    BancoResultRegister == true ? window.location.href = "file:///C:/Users/patrick.piccini/Desktop/TicketManagament2.0/src/main/webapp/home.html"
+    : alert("Erro no Cadastro, Tente Novamente")
+}
